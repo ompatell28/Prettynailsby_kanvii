5331a3ac639e186ba9c5fbcfd62a6d78fb90c11f
ompatell28 <ompatel28107@gmail.com>
2026-06-27 11:59:51 +0530

    added navratri nails and category dropdown list schema

--- my-sanity-project/schemaTypes/portfolio.js
+++ my-sanity-project/schemaTypes/portfolio.js
@@ -5,6 +5,18 @@ export default {
   fields: [
     { name: 'title', title: 'Design Title', type: 'string' },
     { name: 'image', title: 'Upload Image', type: 'image', options: { hotspot: true } },
-    { name: 'category', title: 'Category', type: 'string' }
+    { 
+      name: 'category', 
+      title: 'Category', 
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Press on Nails', value: 'press-on-nails' },
+          { title: 'Nail Extensions', value: 'nail-extensions' },
+          { title: 'Toe Extensions', value: 'toe-extensions' },
+          { title: 'Navratri Nails', value: 'navratri-nails' }
+        ]
+      }
+    }
   ]
 }
